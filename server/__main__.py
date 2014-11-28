5f533309be02536494d14acee2926ec7a9283b6d
Larissa Laich <la8895@web.de>
2014-11-28 22:36:00 +0100

    main.py

--- server/__main__.py
+++ server/__main__.py
@@ -17,17 +17,17 @@ app = Flask(__name__)
 @app.route('/meals/<id>/get/information', methods=['GET'])
 #get guests, date,...
 
-@app.route('/meals/<id>/user/add/<uID>', methods=['POST'])
+@app.route('/meals/<id>/user/add/<userId>', methods=['POST'])
+#payment
 
-
-@app.route('/meals/<id>/user/remove/<uID>', methods=['POST'])
+@app.route('/meals/<id>/user/remove/<userId>', methods=['POST'])
 
 
 @app.route('/meals/search/<latitude>/<longitude>', methods=['GET'])
 	#pass time, typ
 
 @app.route('/rating/host/add/<uhostID>', methods=['POST'])
-	#pass uID
+	#pass userId
 
 
 @app.route('/rating/host/add/<uhostID>', methods=['POST'])
@@ -35,24 +35,37 @@ app = Flask(__name__)
 
 @app.route('/rating/host/average/get/<uhostID>', methods=['GET'])
 
-@app.route('/rating/guest/add/<uID>', methods=['POST'])
+@app.route('/rating/guest/add/<userId>', methods=['POST'])
 	#pass uhostID 
 
-@app.route('/rating/guest/average/get/<uID>', methods=['GET'])
+@app.route('/rating/guest/average/get/<userId>', methods=['GET'])
 	#pass uhostID 
 
 @app.route('/user/create', methods=['POST'])
+def createUser():
+	userId = uuserId.uuserId4()
+	#create User and save it in Database
+	return userId;
 
-@app.route('/user/<uID>/delete', methods=['POST'])
 
-@app.route('/user/<uID>/get/information', methods=['GET'])
-	#name, ratings
+@app.route('/user/<userId>/delete', methods=['POST'])
+def deleteUser(userId):
+	pass
 
-#toAdd: Interface to change User Attributes after Creation
+@app.route('/user/<userId>/get/information', methods=['GET'])
+def getUserInformation():
+#name, ratings
+	pass
+
+@app.route('/user/<userId>/set/information', methods=['POST'])
+def createUser(**kwargs):
+	name = String(request.args.get('name', 'Muster')
+	#toAdd: Interface to change User Attributes after Creation
+	pass
 
 @app.route('/get/food/<latitude>/<longitude>/', methods=['GET'])
 def getLocations(latitude, longitude):
-    pass
+	pass
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
