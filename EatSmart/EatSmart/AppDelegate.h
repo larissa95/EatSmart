e9e5f02c4b4660f656b4a0b9b023a748c52c76f9
Larissa Laich <la8895@web.de>
2014-11-28 20:55:55 +0100

    t

---
+++ EatSmart/EatSmart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EatSmart
+//
+//  Created by Larissa Laich on 28.11.14.
+//  Copyright (c) 2014 Larissa Laich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
