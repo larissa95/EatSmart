aa8df5c99c75f589f293d6b46e93b675ecbd7845
Frederik <f@polarapps.de>
2014-11-28 20:56:34 +0100

    renamed ios folder

---
+++ iOS/EatSmart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EatSmart
+//
+//  Created by Larissa Laich on 28.11.14.
+//  Copyright (c) 2014 Larissa Laich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
