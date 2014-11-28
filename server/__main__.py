6732b82ab64e5f9d0a94bf8cd33bbfc4b71995e7
Timo Müssig <erasio@erasio.de>
2014-11-28 22:35:51 +0100

    added functions

--- server/__main__.py
+++ server/__main__.py
@@ -1,52 +1,68 @@
 import json
 import logging
 import os
-
+import uuid
+ 
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/meals/create', methods=['POST'])
-	#pass user id, datum, meal name,... 
+def meal_create():
+    mealId = uuid.uuid4()
+    meal = {"success":"1", "mealId":mealId}
+    return jsonify(meal)
+    #pass user id, datum, meal name,... 
 
 
 
 @app.route('/meals/<id>/delete', methods=['POST'])
-
+def meal_delete(id):
+    pass
 
 
 @app.route('/meals/<id>/get/information', methods=['GET'])
+def meal_get_information():
+    pass
 #get guests, date,...
 
 @app.route('/meals/<id>/user/add/<uID>', methods=['POST'])
-
+def meal_user_add(userId):
+    pass
 
 @app.route('/meals/<id>/user/remove/<uID>', methods=['POST'])
-
+def meal_user_remove(userId):
+    pass
 
 @app.route('/meals/search/<latitude>/<longitude>', methods=['GET'])
-	#pass time, typ
+def meal_search(latitude, longitude):
+    pass
+    #pass time, typ
 
 @app.route('/rating/host/add/<uhostID>', methods=['POST'])
-	#pass uID
-
-
-@app.route('/rating/host/add/<uhostID>', methods=['POST'])
-
+def rating_host_add(uhostId):
+    pass
+    #pass uID
 
 @app.route('/rating/host/average/get/<uhostID>', methods=['GET'])
+def rating_host_average_get(uhostID):
+    pass
 
 @app.route('/rating/guest/add/<uID>', methods=['POST'])
-	#pass uhostID 
+def rating_guest_add(uID):
+    pass
+    #pass uhostID 
 
 @app.route('/rating/guest/average/get/<uID>', methods=['GET'])
-	#pass uhostID 
+def rating_guest_average_get(uID):
+    pass
+    #pass uhostID 
 
 @app.route('/user/create', methods=['POST'])
 
 @app.route('/user/<uID>/delete', methods=['POST'])
 
 @app.route('/user/<uID>/get/information', methods=['GET'])
-	#name, ratings
+    #name, ratings
 
 #toAdd: Interface to change User Attributes after Creation
 
