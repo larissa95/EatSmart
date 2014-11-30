d28ba42497649090adc8c0f4fbd803f2958dc6d5
Larissa Laich <la8895@web.de>
2014-11-30 09:48:09 +0100

    user meal id relation added

--- iOS/EatSmart/ProfileViewController.m
+++ iOS/EatSmart/ProfileViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ProfileViewController.h"
+#define kPayPalEnvironment PayPalEnvironmentNoNetwork
+
 
 @interface ProfileViewController ()
 
@@ -16,16 +18,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+       /*
+        [PayPalMobile initializeWithClientIdsForEnvironments:@{PayPalEnvironmentSandbox : @"AeEWUBBptPcT0_TaEVdkHM7OWIl3QYQNr6zlsT8Lul_MDjcz_07hbKAV3dqz"}];
+        
+
+     [PayPalMobile initializeWithClientIdsForEnvironments:@{PayPalEnvironmentSandbox : @"AeEWUBBptPcT0_TaEVdkHM7OWIl3QYQNr6zlsT8Lul_MDjcz_07hbKAV3dqz"}];
+     self.payPalConfig = [[PayPalConfiguration alloc] init];
+     self.payPalConfig.acceptCreditCards = YES;
+     self.payPalConfig.languageOrLocale = @"de";
+    [PayPalMobile initializeWithClientIdsForEnvironments:@{PayPalEnvironmentSandbox : @"AeEWUBBptPcT0_TaEVdkHM7OWIl3QYQNr6zlsT8Lul_MDjcz_07hbKAV3dqz"}];
+    self.payPalConfig = [[PayPalConfiguration alloc] init];
+    self.payPalConfig.acceptCreditCards = YES;
+    self.payPalConfig.languageOrLocale = @"en";
+    self.payPalConfig.merchantName = @"Run for it";
+    self.payPalConfig.merchantPrivacyPolicyURL = [NSURL URLWithString:@"https://www.paypal.com/webapps/mpp/ua/privacy-full"];
+    self.payPalConfig.merchantUserAgreementURL = [NSURL URLWithString:@"https://www.paypal.com/webapps/mpp/ua/useragreement-full"];
+    
+    UIButton *loginButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    loginButton.frame = CGRectMake(70, self.view.frame.size.height-150, self.view.frame.size.width-140, 60);
+    [loginButton addTarget:self action:@selector(getUserAuthorizationForProfileSharing:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIImage *btnImage = [UIImage imageNamed:@"loginWithPayPal.png"];
+    
+    [loginButton setBackgroundImage:btnImage forState:UIControlStateNormal];
+    loginButton.contentMode=UIViewContentModeScaleAspectFit;
+    
+    
+    //[loginButton setTitle:@"Login With Paypal" forState:UIControlStateNormal];
+    [self.view addSubview:loginButton];
+    self.navigationController.interactivePopGestureRecognizer.enabled = NO;
+    [self.navigationItem setHidesBackButton:YES];
+
+    */
+    
+    
+    
 
     if(![LocalDataBase UserIsRegistered]){
         [self loadUserIdFromServer];
     }else{
         NSLog(@"%i",[LocalDataBase userId]);
-      /*  ServerCommunication *server = server = [[ServerCommunication alloc] init];
+        ServerCommunication *server = server = [[ServerCommunication alloc] init];
         server.delegate = self;
         server.tag = 1;
         [server loadDataFromServerWithURL:[NSString stringWithFormat:@"%@/user/%i/information",[ServerUrl serverUrl],[LocalDataBase userId]] andParameters:@"" andHTTPMethod:@"GET"];
-*/
+
         /*
         def getUserInformation(userId):
          hostRating = calculateAverageHostRating(userId)
@@ -85,6 +123,51 @@
     //block
 }
 
+/*
+- (IBAction)getUserAuthorizationForProfileSharing:(id)sender {
+    
+    NSSet *scopeValues = [NSSet setWithArray:@[kPayPalOAuth2ScopeOpenId, kPayPalOAuth2ScopeEmail, kPayPalOAuth2ScopeAddress, kPayPalOAuth2ScopePhone]];
+    
+    PayPalProfileSharingViewController *profileSharingPaymentViewController = [[PayPalProfileSharingViewController alloc] initWithScopeValues:scopeValues configuration:self.payPalConfig delegate:self];
+    [self presentViewController:profileSharingPaymentViewController animated:YES completion:nil];
+}
+
+
+
+- (void)payPalProfileSharingViewController:(PayPalProfileSharingViewController *)profileSharingViewController
+             userDidLogInWithAuthorization:(NSDictionary *)profileSharingAuthorization {
+    NSLog(@"PayPal Profile Sharing Authorization Success!");
+    
+    // self.resultText = [profileSharingAuthorization description];
+    //[self showSuccess];
+    [self sendProfileSharingAuthorizationToServer:profileSharingAuthorization];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)userDidCancelPayPalProfileSharingViewController:(PayPalProfileSharingViewController *)profileSharingViewController {
+    NSLog(@"PayPal Profile Sharing Authorization Canceled");
+    [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
+    //    self.successView.hidden = YES;
+    
+    [self loginWithUser:[ServerDataBaseCommunication userForUUID:@"larissa"]];
+    ChooseCharityViewController *charity = [[ChooseCharityViewController alloc] initWithUser:[ServerDataBaseCommunication userForUUID:@"larissa"]];
+    [self.navigationController pushViewController:charity animated:YES];
+    
+ 
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)sendProfileSharingAuthorizationToServer:(NSDictionary *)authorization {
+    // TODO: Send authorization to server
+ 
+    [self loginWithUser:[ServerDataBaseCommunication userForUUID:@"larissa"]];
+    ChooseCharityViewController *charity = [[ChooseCharityViewController alloc] initWithUser:[ServerDataBaseCommunication userForUUID:@"larissa"]];
+    [self.navigationController pushViewController:charity animated:YES];
+    
+ 
+    NSLog(@"Here is your authorization:\n\n%@\n\nSend this to your server to complete profile sharing setup.", authorization);
+}
+*/
 /*
 #pragma mark - Navigation
 
